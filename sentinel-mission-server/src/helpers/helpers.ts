59e63e5c909a8cb2d6350a6402e0437e5993b838
DvirShahala <61511562+DvirShahala@users.noreply.github.com>
2022-12-15 16:15:38 +0200

    add logger

--- sentinel-mission-server/src/helpers/helpers.ts
+++ sentinel-mission-server/src/helpers/helpers.ts
@@ -1,6 +1,7 @@
 import Axios from "axios";
 import { IGetImagesRes } from "../types/types";
 import { CacheService } from "../cache/cacheService";
+import { logger } from "../index";
 
 const israelPolygon =
   "35.1069403 33.0752527, 34.7799253 32.1044979, 34.5490193 31.6681206, 34.2390203 31.2994915, 34.9180055 29.5278176, 35.429213 31.0814227, 35.5623579 32.640459, 35.7555628 32.7465508, 35.8809614 32.9175661, 35.7710981 33.3040622, 35.5678511 33.2305751, 35.5293989 33.0972222, 35.3755903 33.028167, 35.2767134 33.0880179, 35.1069403 33.0752527";
@@ -30,14 +31,14 @@ export const fetchRandomImage = async () => {
       Number(process.env.MAX_RANDOM_NUMBER) ?? 101
     );
 
-    console.log("random number -", randomStart);
+    logger.info(`random number - ${randomStart}`);
 
     if (cache.getByKey(String(randomStart))) {
-      console.log("Fetching from cache");
+      logger.info("Fetching from cache");
 
       return cache.getByKey(String(randomStart)).cacheData;
     } else {
-      console.log("Fetching from api");
+      logger.info("Fetching from api");
 
       const { data } = await Axios.get<IGetImagesRes>(
         `${process.env.OPEN_SEARCH_HUB_BASE_URL}?q=footprint:"Intersects(POLYGON((${israelPolygon})))"cloudcoverpercentage: [${process.env.CLOUD_COVER_PERCENTAGE}]platformname:${process.env.PLATFORM_NAME}&rows=${process.env.ROWS}&start=${randomStart}&format=${process.env.FORMAT}`,
@@ -62,7 +63,7 @@ export const fetchRandomImage = async () => {
       }
     }
   } catch (e) {
-    console.error("error in getRandomImage", e);
+    logger.error("error in getRandomImage", e);
     throw e;
   }
 };
--- sentinel-mission-server/src/index.ts
+++ sentinel-mission-server/src/index.ts
@@ -1,7 +1,22 @@
 import app from "./config/app";
 import * as dotenv from "dotenv";
+import winston from "winston";
 
 dotenv.config();
+
+export const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.Console({
+      level: process.env.LOG_LEVEL,
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.simple()
+      ),
+    }),
+  ],
+});
 
 app.listen(process.env.PORT, () => {
   console.log("Express server listening on port " + process.env.PORT);
